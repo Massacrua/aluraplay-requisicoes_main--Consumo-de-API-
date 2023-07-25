8d1232d078b007c57ea15f3ae625621c7bc2ed54
Guilherme Dalla Rosa <gui.dallarosa@gmail.com>
2023-07-25 17:40:31 -0300

    initial commit

---
+++ js/conectaApi.js
@@ -0,0 +1,44 @@
+const APIVideos = "http://localhost:3000/videos"
+
+async function listaVideos() {
+    const conexao = await fetch(APIVideos)
+    const conexaoConvertida = await conexao.json()
+   
+    return conexaoConvertida
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch(APIVideos, {
+        method: "POST", 
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = await conexao.json()
+
+    return conexaoConvertida
+}
+
+listaVideos()
+
+export const conectaApi = {
+    listaVideos,
+    criaVideo,
+    buscaVideo
+}
